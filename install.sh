4b35c3c76224cb9e408fc8e3ad036e6baf196266
zxcv1246789 <zxcv1246789@naver.com>
2018-02-19 14:13:48 +0900

    test

--- install.sh
+++ install.sh
@@ -4,17 +4,11 @@ cd packages
 
 for id in $(ls) do
 
-  if [ $id = "ls" ]
-  then
-    echo "exittt";
-    exit
-  else
     echo $id;
     cd $id;
     zip $id.zip -r ./*
     mv "$id.zip" ..
     cd ..
     echo "out: $id.zip"
-  fi
 
 done
