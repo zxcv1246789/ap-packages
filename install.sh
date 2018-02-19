cf0f3c92449b2fe0802417ba7367480740e75da6
zxcv1246789 <zxcv1246789@naver.com>
2018-02-19 14:28:14 +0900

    test

--- install.sh
+++ install.sh
@@ -7,11 +7,10 @@ for id in $(ls); do
     echo $id;
     cd $id;
     #zip $id.zip -r ./*;
-    $ AA = 'mv'
-    $ BB = "$id.zip"
-    $ CC = '..'
-
-    $ echo "$AA $BB $CC"
+    AA = 'mv'
+    BB = "$id.zip"
+    CC = '..'
+    echo "$AA $BB $CC"
     cd ..
     echo "out: $id.zip"
 
