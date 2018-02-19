e9400ea3aa8545f7dcef4c1daa1492cf3d0a16d1
zxcv1246789 <zxcv1246789@naver.com>
2018-02-19 14:06:21 +0900

    test

--- install.sh
+++ install.sh
@@ -4,11 +4,16 @@ cd packages
 
 for id in 'ls'; do
 
-  echo $id;
-  cd $id;
-  zip $id.zip -r ./*
-  mv "$id.zip" ..
-  cd ..
-  echo "out: $id.zip"
+  if [ $id = "ls" ]
+  then
+    exit
+  else
+    echo $id;
+    cd $id;
+    zip $id.zip -r ./*
+    mv "$id.zip" ..
+    cd ..
+    echo "out: $id.zip"
+  fi
 
 done
