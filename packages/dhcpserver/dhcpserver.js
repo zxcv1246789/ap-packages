a559b63cbc21beeb00b8fca171e78fefad1bc694
zxcv1246789 <zxcv1246789@naver.com>
2018-02-22 15:22:24 +0900

    test

--- packages/dhcpserver/dhcpserver.js
+++ packages/dhcpserver/dhcpserver.js
@@ -1,11 +1,11 @@
 var fs = require("fs");
 var exec = require('child_process').exec,
   child;
-  const {
-    execSync
-  } = require('child_process');
+const {
+  execSync
+} = require('child_process');
 
-exports.api_get = function(req, res) {
+exports.api_get = function() {
   const stdout2 = execSync('cat /etc/dnsmasq.conf', {
     encoding: 'utf8'
   });
@@ -15,22 +15,11 @@ exports.api_get = function(req, res) {
   var range = tmp[1].split(",");
   range[2] = range[2].match(/([0-9]*)([a-z])/i);
   var qwe = range[2][0].match(/([0-9]*)/i);
-  exports.savedata_serversetting(req, res, interface_type[1], range, qwe[0]);
-
+  let data = exports.savedata_serversetting(interface_type[1], range, qwe[0]);
+  return data;
 }
-exports.api_get_awk = function(req, res) {
 
-  child = exec("ip -o link show | awk -F': ' '{print $2}'", function(error, stdout, stderr) {
-    var arr = stdout.split("\n");
-    var awkdata = {};//오브젝트
-    for(var a = 0;a < arr.length;a++){
-      awkdata[a] = arr[a];
-    }
-    res.send(awkdata);
-
-  });
-}
-exports.savedata_serversetting = function(req, res, interface_type, range, interval) {
+exports.savedata_serversetting = function(interface_type, range, interval) {
   var s_setting_data = {}; //오브젝트
   s_setting_data["interface"] = interface_type;
   s_setting_data["starting_IP_address"] = range[0];
@@ -38,91 +27,99 @@ exports.savedata_serversetting = function(req, res, interface_type, range, inter
   s_setting_data["Lease_time"] = interval;
   s_setting_data["interval"] = range[2][2];
   // SAVE DATA
-  fs.writeFile(__dirname + "/data/" + "serversetting.json",
+  fs.writeFileSync(__dirname + "/data/" + "serversetting.json",
     JSON.stringify(s_setting_data, null, '\t'), "utf8",
     function(err, data) {
       result = {
         "success": 1
       };
-      res.json(s_setting_data);
     })
+  return s_setting_data;
 }
-
-exports.api_get_clientlist = function(req, res) {
-  exports.write_clientlist(req, res);
-}
-exports.write_clientlist = function(req, res) {
-  child = exec("cat /var/lib/misc/dnsmasq.leases", function(error, stdout, stderr) {
-    console.log('stdout: ' + stdout[11]);
-
-    var arr = []; //줄 단위로 배열 저장(마지막은 빈배열이 들어감.)
-    arr = stdout.split("\n");
-    var data__ = {};
-    for (var i = 0; i < arr.length - 1; i++) { //2차원 배열에 data 저장
-      arr[i] = arr[i].split(" ");
-      var tmp = {};
-      var string_num = "client_list_";
-      string_num += String(i + 1);
-      tmp['Expire time'] = arr[i][0];
-      tmp['MAC Address'] = arr[i][1];
-      tmp['IP Address'] = arr[i][2];
-      tmp['Host name'] = arr[i][3];
-      tmp['Client ID'] = arr[i][4];
-      data__[string_num] = tmp;
-      console.log('arr ' + i + ' : ' + arr[i]);
-      console.log('arr ' + i + '[] : ' + arr[i][0]);
-    }
-    fs.writeFile(__dirname + "/data/" + "clientlist.json",
-      JSON.stringify(data__, null, '\t'), "utf8",
-      function(err, data) {
-        result = {
-          "success": 1
-        };
-        res.json(data__);
-      })
+exports.api_get_awk = function() {
+  const stdout = execSync("ip -o link show | awk -F': ' '{print $2}'", {
+    encoding: 'utf8'
   });
+  var arr = stdout.split("\n");
+  var awkdata = {}; //오브젝트
+  for (var a = 0; a < arr.length; a++) {
+    awkdata[a] = arr[a];
+  }
+  return awkdata;
 }
-exports.api_get_dnsmasq = function(req, res) {
-  exports.write_pidof_dnsmasq(req, res);
-}
-
-exports.write_pidof_dnsmasq = function(req, res) {
-  child = exec("pidof dnsmasq | wc -l", function(error, stdout, stderr) {
-    var data = {}; //오브젝트
-    if (stdout[0] == 0) {
-      data['Dnsmasq is'] = false;
-    } else if (stdout[0] == 1) {
-      data['Dnsmasq is'] = true;
-    }
-    var dnsmasqdata = {};
-    dnsmasqdata['alert_select'] = data;
-
-    fs.writeFile(__dirname + "/data/" + "dnsmasq_dec.json",
-      JSON.stringify(dnsmasqdata, null, '\t'), "utf8",
-      function(err, data) {
-        result = {
-          "success": 1
-        };
-        res.json(dnsmasqdata);
-      })
+exports.api_get_clientlist = function() {
+  const stdout = execSync("cat /var/lib/misc/dnsmasq.leases", {
+    encoding: 'utf8'
   });
+  console.log('stdout: ' + stdout[11]);
+
+  var arr = []; //줄 단위로 배열 저장(마지막은 빈배열이 들어감.)
+  arr = stdout.split("\n");
+  var data__ = {};
+  for (var i = 0; i < arr.length - 1; i++) { //2차원 배열에 data 저장
+    arr[i] = arr[i].split(" ");
+    var tmp = {};
+    var string_num = "client_list_";
+    string_num += String(i + 1);
+    tmp['Expire time'] = arr[i][0];
+    tmp['MAC Address'] = arr[i][1];
+    tmp['IP Address'] = arr[i][2];
+    tmp['Host name'] = arr[i][3];
+    tmp['Client ID'] = arr[i][4];
+    data__[string_num] = tmp;
+    console.log('arr ' + i + ' : ' + arr[i]);
+    console.log('arr ' + i + '[] : ' + arr[i][0]);
+  }
+  let data = exports.write_clientlist(data__);
+  return data;
 }
-
-exports.api_post = function(req, res) {
-  req.accepts('application/json');
-  // input message handling
-  json = req.body;
-
-  // output message
-  fs.writeFile(__dirname + "/data/" + "serversetting.json",
-    JSON.stringify(json, null, '\t'), "utf8",
+exports.write_clientlist = function(data__) {
+  fs.writeFileSync(__dirname + "/data/" + "clientlist.json",
+    JSON.stringify(data__, null, '\t'), "utf8",
     function(err, data) {
       result = {
         "success": 1
       };
-      exports.tmp_file_save();
-      res.json(result);
     })
+  return data__;
+}
+exports.api_get_dnsmasq = function() {
+  const stdout = execSync("pidof dnsmasq | wc -l", {
+    encoding: 'utf8'
+  });
+  var data = {}; //오브젝트
+  if (stdout[0] == 0) {
+    data['Dnsmasq is'] = false;
+  } else if (stdout[0] == 1) {
+    data['Dnsmasq is'] = true;
+  }
+  var dnsmasqdata = {};
+  dnsmasqdata['alert_select'] = data;
+  exports.write_pidof_dnsmasq(dnsmasqdata);
+  return dnsmasqdata;
+}
+
+exports.write_pidof_dnsmasq = function(dnsmasqdata) {
+
+  fs.writeFileSync(__dirname + "/data/" + "dnsmasq_dec.json",
+    JSON.stringify(dnsmasqdata, null, '\t'), "utf8",
+    function(err, data) {
+      result = {
+        "success": 1
+      };
+    })
+}
+
+exports.api_post = function(json) {
+  var result = new Object();
+  // output message
+  fs.writeFileSync(__dirname + "/data/" + "serversetting.json",
+    JSON.stringify(json, null, '\t'), "utf8",
+    function(err, data) {
+      result.success = 1;
+      exports.tmp_file_save();
+    })
+  return result;
 }
 
 exports.tmp_file_save = function() {
@@ -139,7 +136,6 @@ exports.tmp_file_save = function() {
   text_tmp += "bogus-priv\n";
   text_tmp += "dhcp-range=" + serversetting_data['starting_IP_address'] + "," + serversetting_data['ending_IP_address'] + "," + serversetting_data['Lease_time'] + "" + serversetting_data['interval'];
 
-
   fs.writeFileSync(__dirname + "/data/" + "tmp.txt",
     text_tmp, "utf8",
     function(err, data) {
@@ -152,37 +148,36 @@ exports.tmp_file_save = function() {
   });
 }
 
-exports.start_stopbutton = function(req, res) {
-  child = exec("pidof dnsmasq | wc -l", function(error, stdout, stderr) {
-    res.send(stdout);
+exports.start_stopbutton = function() {
+  const stdout = execSync("pidof dnsmasq | wc -l", {
+    encoding: 'utf8'
   });
+  return stdout;
 }
 
-exports.dnsmasq_stop = function(req, res) {
+exports.dnsmasq_stop = function() {
+  result = {
+    "success": 0
+  };
   child = exec("sudo /etc/init.d/dnsmasq stop", function(error, stdout, stderr) {
-    result = {
-      "success": 0
-    };
-    res.send(result);
   });
+  return result;
 }
-exports.dnsmasq_start = function(req, res) {
+exports.dnsmasq_start = function() {
+  result = {
+    "success": 1
+  };
   child = exec("sudo /etc/init.d/dnsmasq start", function(error, stdout, stderr) {
-    result = {
-      "success": 1
-    };
-    res.send(result);
   });
+  return result;
 }
 
-exports.i18n_load = function(req, res) {
+exports.i18n_load = function() {
   var data = JSON.parse(fs.readFileSync(__dirname + "/../../public/i18n/config.js", 'utf8'));
   console.log(data);
-  res.send(data);
+  return data;
 }
-
-exports.i18n_save = function(req, res) {
-  var language = req.query.lang;
+exports.i18n_save = function(language) {
   var lang_json = {};
   lang_json.language = language;
   fs.writeFileSync(__dirname + "/../../public/i18n/config.js",
@@ -192,5 +187,5 @@ exports.i18n_save = function(req, res) {
         "success": 1
       };
     })
-  res.send(language);
+  return language;
 }
--- packages/dhcpserver/main.js
+++ packages/dhcpserver/main.js
@@ -17,31 +17,42 @@ module.exports = function(app, fs, url){
     var type = req.query.type;
     var select = req.query.select;
     if (type == "clientlist") {
-      router_dhcpserver.api_get_clientlist(req, res);
+      let data = router_dhcpserver.api_get_clientlist();
+			res.send(data);
     } else if (type == "dnsmasq") {
-      router_dhcpserver.api_get_dnsmasq(req, res);
+      let data = router_dhcpserver.api_get_dnsmasq();
+			res.send(data);
     } else if (type == "get") {
-      router_dhcpserver.api_get(req, res);
+      let data = router_dhcpserver.api_get();
+			res.send(data);
     } else if (type == "awk") {
-      router_dhcpserver.api_get_awk(req, res);
+      let data = router_dhcpserver.api_get_awk();
+			res.send(data);
     } else if (type == "dnsstopstart") {
       if (select == 0) { //stop 시키는 부분
-        router_dhcpserver.dnsmasq_stop(req, res);
+        let data = router_dhcpserver.dnsmasq_stop();
+				res.send(data);
       } else if (select == 1) { //start 시키는 부분
-        router_dhcpserver.dnsmasq_start(req, res);
+        let data = router_dhcpserver.dnsmasq_start();
+				res.send(data);
       } else {
-        router_dhcpserver.start_stopbutton(req, res);
+        let data = router_dhcpserver.start_stopbutton();
+				res.send(data);
       }
     }
   });
   app.post('/api/dhcpserver', function(req, res) {
-    router_dhcpserver.api_post(req, res);
+		req.accepts('application/json');
+  	let data = router_dhcpserver.api_post(req.body);
+		res.send(data);
   });
 	app.get('/i18n_load', function(req, res) {
-    router_dhcpserver.i18n_load(req, res);
+    let data = router_system.i18n_load();
+		res.send(data);
   });
-	app.get('/i18n_save', function(req, res) {
-    router_dhcpserver.i18n_save(req, res);
-  });
+  app.get('/i18n_save', function(req, res) {
+    let data = router_system.i18n_save(req.query.lang);
+		res.send(data);
+	});
 
 };
